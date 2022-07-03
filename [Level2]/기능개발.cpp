4f1e8664384f968e1149c22b35628b37d64ee47d
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-04 02:09:33 +0900

    Update 기능개발.cpp

--- [Level2]/기능개발.cpp
+++ [Level2]/기능개발.cpp
@@ -26,25 +26,27 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     }
 
     int day = workingday.front(); // 처음 작업 일수를 기준 값으로 설정 
-    int popcnt = 0;
+    
     for (int k = 0; k < len; ++k) {
         if (day >= workingday[k]) {
-            // 배포 가능
+            // 배포 가능하여 큐에 넣는다.
             success_work.push(workingday[k]);
         }
         else {
-            // 배포 불가능
+            // 배포 불가능, 이전 작업을 완료해야함
             answer.push_back(success_work.size());
 
+            // 이전 작업 목록 초기화
             while (!success_work.empty()) {
                 success_work.pop();
             }
 
             day = workingday[k]; // 기준 값 변경
-            success_work.push(workingday[k]);
+            success_work.push(workingday[k]); // 배포 가능하여 큐에 넣는다.
         }
     }
 
+    // 마지막 반복문의 경우 이전 작업을 완료하는 부분을 하지 못함으로 answer에 큐 사이즈만큼 push_back 해준다.
     answer.push_back(success_work.size());
    
 
