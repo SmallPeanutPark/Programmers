736eea09d9ca51e5d43c980c82757f0d5adf05ae
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-04 01:42:58 +0900

    Update 기능개발.cpp

--- [Level2]/기능개발.cpp
+++ [Level2]/기능개발.cpp
@@ -17,38 +17,36 @@ int calculateday(int working, int speed) {
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    queue<int> workingday; // 작업 일 수 저장하기 위한 queue
+    vector<int> workingday; // 작업 일 수 저장하기 위한 vector
+    queue<int> success_work;
 
     int len = progresses.size();
     for (int k = 0; k < len; ++k) {
-        workingday.push(calculateday(progresses[k], speeds[k])); // 작업 일수 계산
+        workingday.push_back(calculateday(progresses[k], speeds[k])); // 작업 일수 계산
     }
 
     int day = workingday.front(); // 처음 작업 일수를 기준 값으로 설정 
     int popcnt = 0;
-    while (!workingday.empty()) {
-        workingday.pop();
-        if (!workingday.empty()) {
-            if (day < workingday.front()) {
-                // 같이 배포 불가능
-                popcnt++;
-                answer.push_back(popcnt);
-                day = workingday.front();
-                popcnt = 0;
-            }
-            else {
-                // 같이 배포 가능
-                // day 값을 유지 해야함
-                popcnt++;
-                continue;
-            }
+    for (int k = 0; k < len; ++k) {
+        if (day >= workingday[k]) {
+            // 배포 가능
+            success_work.push(workingday[k]);
         }
         else {
-            popcnt++;
-            answer.push_back(popcnt);
-            popcnt = 0;
+            // 배포 불가능
+            answer.push_back(success_work.size());
+
+            while (!success_work.empty()) {
+                success_work.pop();
+            }
+
+            day = workingday[k]; // 기준 값 변경
+            success_work.push(workingday[k]);
         }
     }
+
+    answer.push_back(success_work.size());
+   
 
     return answer;
 }
