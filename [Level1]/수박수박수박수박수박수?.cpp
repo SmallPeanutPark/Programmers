e9ba8c17cef0e8073fd61d770b6b34fd5769c69a
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-09 22:41:35 +0900

    Create 수박수박수박수박수박수?.cpp
    Programmers [Level1] 수박수박수박수박수박수?

---
+++ [Level1]/수박수박수박수박수박수?.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+   string answer = "";
+        
+    for (int i = 1; i <= n; ++i)
+    {
+        if (i % 2 == 0)
+        {
+            answer += "박";
+        }
+        else
+        {
+            answer += "수";
+        }
+    }
+
+    return answer;
+}
