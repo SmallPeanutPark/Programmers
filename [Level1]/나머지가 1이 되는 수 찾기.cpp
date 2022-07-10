376d8d120d74af4845fb33d14ada582f4130bb33
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-10 13:11:24 +0900

    Create 나머지가 1이 되는 수 찾기.cpp
    Programmers [Level1] 나머지가 1이 되는 수 찾기

---
+++ [Level1]/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+
+    while (true) {
+        if (n % answer == 1) {
+            break;
+        }
+        ++answer;
+    }
+    return answer;
+}
