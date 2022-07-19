369f1fd63f9f1be7d9443753634c975fe9ba9edd
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-20 08:49:08 +0900

    Create N개의 최소공배수.cpp
    Programmers [Level2] N개의 최소공배수

---
+++ [Level2]/N개의 최소공배수.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+// 최대 공약수 구하기
+int gcd(int a, int b) {
+    
+    int r = 0;
+    while(true) {
+        r = a % b;
+        if(r == 0) break;
+        a = b;
+        b = r;
+    }
+
+    return b;
+}
+
+// 최소 공배수 구하기
+int lcd(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+
+int solution(vector<int> arr) {
+    int answer = 0;
+    // 최대 공약수를 구하고 최소 공배수를 구할 수 있다.
+    sort(arr.begin(), arr.end(), greater<int>()); // 내림 차순 정렬
+
+    int len = arr.size();
+    answer = arr[0];
+    for(int i = 1; i < len; ++i) {
+        answer = lcd(answer, arr[i]);
+    }
+
+    return answer;
+}
