f61235e03627925bddba13b0008d6a0d895add70
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-08-02 13:56:52 +0900

    Create 피로도.cpp
    Programmers [Level2] 피로도

---
+++ [Level2]/피로도.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = 0;
+    int result = 0;
+    int len = dungeons.size();
+    sort(dungeons.begin(), dungeons.end());
+    do {
+        int temp = k;
+        int answer = 0;
+        for (int i = 0; i < len; ++i) {
+            if (dungeons[i][0] <= temp) {
+                temp -= dungeons[i][1];
+                answer += 1;
+            }
+        }
+        result = max(result, answer);
+    } while (next_permutation(dungeons.begin(), dungeons.end()));
+    return result;
+}
