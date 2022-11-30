ff5d7adb535cd66bcb9c7a2a1466e97e8df793ba
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-11-30 09:22:18 +0900

    Create 귤 고르기.cpp
    Programmers [Level2] 귤 고르기

---
+++ [Level2]/귤 고르기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+using namespace std;
+
+vector<int> box;
+
+void setBox(vector<int>& t) {
+    for(int i = 0; i < t.size(); ++i) {
+        box[t[i]-1] += 1;
+    }
+}
+
+int solution(int k, vector<int> tangerine) {
+    sort(tangerine.begin(), tangerine.end(), greater<int>());
+    box.resize(tangerine[0]);
+    setBox(tangerine);
+    sort(box.begin(), box.end(), greater<int>());
+    int answer = 0;
+    for(int element : box) {
+        if(k <= 0) break;
+        answer +=1;
+        k -= element;
+    }
+    return answer;
+}
