2becf16662568026a3bc682806debe299090d759
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-12-08 15:29:03 +0900

    Update 오픈채팅방.cpp
    함수명 변경, 불필요한 조건문 삭제

--- [Level2]/오픈채팅방.cpp
+++ [Level2]/오픈채팅방.cpp
@@ -8,7 +8,7 @@ using namespace std;
 map<string, string> id;
 vector<string> info = {"Enter", "Leave", "Change"}; // 들어옴, 떠남, 변경함
 
-void divideStr(vector<string> r, int len) {
+void changeID(vector<string> r, int len) {
     for(int i = 0; i < len; ++i) {
         stringstream ss(r[i]);
         string token = "";
@@ -35,7 +35,7 @@ void divideStr(vector<string> r, int len) {
 vector<string> solution(vector<string> record) {
     vector<string> answer;
     int len = record.size();
-    divideStr(record, len);
+    changeID(record, len);
     for(int i = 0; i < len; ++i) {
         stringstream ss(record[i]);
         string token = "";
@@ -48,18 +48,14 @@ vector<string> solution(vector<string> record) {
         
         if(info[0].compare(s[0]) == 0) {
             // Enter
-           if(id.find(s[1]) != id.end()) {
-                string str = id[s[1]];
-                str += "님이 들어왔습니다.";
-                answer.emplace_back(str);
-           }
+            string str = id[s[1]];
+            str += "님이 들어왔습니다.";
+            answer.emplace_back(str);
         } else if(info[1].compare(s[0]) == 0) {
             // Leave
-            if(id.find(s[1]) != id.end()) {
-                string str = id[s[1]];
-                str += "님이 나갔습니다.";
-                answer.emplace_back(str);
-            }
+            string str = id[s[1]];
+            str += "님이 나갔습니다.";
+            answer.emplace_back(str);
         } else {
             // do nothing
             continue;
